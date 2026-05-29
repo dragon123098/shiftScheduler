f7d3d996db7f9ab663aa79d8e7e260965c726bb9
Peter Grunig <tanna435@byu.edu>
2026-05-29 16:58:19 -0600

    attempt to have ai wright some CSS

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"net/http"
+	"html/template"
 	"log"
+	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World ALIENS TEST"))
+	ts, err := template.ParseFiles("home.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 
 func main() {
 	mux := http.NewServeMux()
+	
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	
 	mux.HandleFunc("/{$}", home)
 
 	err := http.ListenAndServe(":4000", mux)
