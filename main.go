7af86824c306653ebd9182171bbc39e67a579114
Peter Grunig <petergrunig@gmail.com>
2026-05-29 18:03:44 -0600

    Now it works. Its a basic site

--- main.go
+++ main.go
@@ -4,17 +4,22 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"fmt"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("home.html")
+	ts, err := template.ParseFiles("./templates/home.html")
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println("Error parsing template:", err)
+
 		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println("Error executing template:", err)
+
 		return
 	}
 }
@@ -25,8 +30,11 @@ func main() {
 	
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	
-	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/", home)
 
+	fmt.Println("Server is running on http://localhost:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+	
+
 }
