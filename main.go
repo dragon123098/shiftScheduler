7fa91c69cffe046339ccf4c992c360bc95b96916
Peter Grunig <petergrunig@gmail.com>
2026-05-30 12:23:09 -0600

    Saves input from user to a text file

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"fmt"
+	"os"
 )
 
+// Defines a user struct to hold the user's name, password and schedule
+// type User struct {
+// 	Name     string
+// 	Password string
+// 	Schedule Schedule
+// }
+
+// Home handler to render the home page
 func home(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles("./templates/home.html")
 	if err != nil {
@@ -24,17 +34,48 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Save handler to process form input
+func save(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var data map[string]string
+	json.NewDecoder(r.Body).Decode(&data)
+
+	inputValue := data["data"]
+
+	// Write input value to schedule.txt
+	file, err := os.OpenFile("schedule.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		http.Error(w, "Failed to save data", http.StatusInternalServerError)
+		log.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(inputValue + "\n")
+	if err != nil {
+		http.Error(w, "Failed to save data", http.StatusInternalServerError)
+		log.Println("Error writing to file:", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"status": "success"})
+}
 
 func main() {
 	mux := http.NewServeMux()
-	
+
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	
+
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/save", save)
 
 	fmt.Println("Server is running on http://localhost:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-	
 
 }
